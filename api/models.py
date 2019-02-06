24842d47aa357484c46ebdd4bcbd3b8e79250f6f
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2019-02-07 04:28:59 +0530

    Add Host and HostParameter models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Host(models.Model):
+    """
+    Any entity on a network capable of SNMP
+    """
+    mac_addr = models.CharField(max_length=100, null=True, blank=False)
+    current_ip = models.CharField(max_length=15, null=True, blank=False)
+
+    def __str__(self):
+        return self.mac_addr
+
+
+class HostParameter(models.Model):
+    """
+    Model to store individual parameters i.e. info about a particular host.
+    """
+    host = models.ForeignKey(Host, on_delete=models.CASCADE)
+    oid = models.CharField(max_length=100, null=True, blank=True)
+    value = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.host.mac_addr + "(" + self.oid + ")"
