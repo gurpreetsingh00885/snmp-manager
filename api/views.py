89637a01f8395d6f664fc8f3d21f7e9753837a3b
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2019-02-07 04:41:39 +0530

    Configure view to create/update Host

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from snmp_manager.users.models import User
 from rest_framework import viewsets
 from rest_framework import views
+from api.models import Host, HostParameter
 from api.serializers import UserSerializer
 from rest_framework.response import Response
 
@@ -26,7 +27,24 @@ class CreateOrUpdateHost(views.APIView):
     information about a certain host.
     """
     def post(self, request, format=None):
-        print(request.data)
-        return Response(request.data)
+        variables = request.data["vars"]
+        temp = {}
+        for oid, val in [x.split("<is>") for x in variables.split(',')]:
+            temp[oid] =  val
+        mac_addr = temp.pop("IF-MIB::ifPhysAddress")
+        host, created = Host.objects.get_or_create(mac_addr=mac_addr)
+
+        if created:
+            for oid in temp:
+                HostParameter.objects.create(oid=oid,
+                                             value=temp[oid],
+                                             host=host)
+        else:
+            for oid in temp:
+                param = HostParameter.objects.get(oid=oid, host=host)
+                param.value = temp[oid]
+                param.save()
+
+        return Response({"status" : "successful"})
 
 create_update_host = CreateOrUpdateHost.as_view()
